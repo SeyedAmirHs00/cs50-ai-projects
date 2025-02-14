e9a76b446ee802fbd1b85e51b35d795e88d16e7f
amir <seyedamirhosseini00@gmail.com>
2025-02-14 06:52:56 +0330

    Complete TicTacToe project

---
+++ projects0/tictactoe/README.md
@@ -0,0 +1,24 @@
+# TicTacToe project
+
+This project is about TicTacToe game, We have an AI player that uses minimax
+algorithm and search over game tree to find best move
+
+
+## How to run
+
+Run this project by first installing the requirements.txt dependencies
+
+```bash
+pip install -r requirements.txt
+```
+
+then execute `runner.py` file with the command `python runner.py`
+
+
+## How to test
+
+Test this project by the fallowing command, after installing `check50` with pip
+
+```bash
+check50 ai50/projects/2024/x/tictactoe
+```
--- projects0/tictactoe/tictactoe.py
+++ projects0/tictactoe/tictactoe.py
@@ -2,8 +2,9 @@
 Tic Tac Toe Player
 """
 
-import math
+import copy
 
+N = 3
 X = "X"
 O = "O"
 EMPTY = None
@@ -22,46 +23,146 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    count_of_O = 0
+    count_of_X = 0
+    for row in board:
+        for element in row:
+            if element == X:
+                count_of_X += 1
+            elif element == O:
+                count_of_O += 1
+    if count_of_X == count_of_O + 1:
+        return O
+    else:
+        return X
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    ret = set()
+    for i, row in enumerate(board):
+        for j, element in enumerate(row):
+            if element == EMPTY:
+                ret.add((i, j))
+    return ret
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    player_element = player(board)
+    i, j = action
+    if board[i][j] != EMPTY:
+        raise ValueError("Selected action is not primited")
+    if not (0 <= i < N and 0 <= j < N):
+        raise ValueError("Selected action parts should not be negetive nor greater than 2")
+    ret_board = copy.deepcopy(board)
+    ret_board[i][j] = player_element
+    return ret_board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    for i in range(3):
+        if board[i][0] == EMPTY:
+            continue
+        is_equal = True
+        for j in range(3):
+            if board[i][0] != board[i][j]:
+                is_equal = False
+        if is_equal:
+            return board[i][0]
+
+    for j in range(3):
+        if board[0][j] == EMPTY:
+            continue
+        is_equal = True
+        for i in range(3):
+            if board[0][j] != board[i][j]:
+                is_equal = False
+        if is_equal:
+            return board[0][j]
+
+    if board[0][0] == board[1][1] == board[2][2]:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0]:
+        return board[0][2]
+
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board) is not None:
+        return True
+    
+    # Check if the board is full
+    for row in board:
+        for element in row:
+            if element == EMPTY:
+                return False
+    return True
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    winner_player = winner(board)
+    if winner_player == X:
+        return 1
+    elif winner_player == O:
+        return -1
+    else:
+        return 0
+
+
+def max_value(board):
+    if player(board) != X:
+        raise ValueError("Player should be X in max_value")
+    if terminal(board):
+        return (utility(board), None)
+    available_actions = actions(board)
+    best_action = None
+    best_action_val = -2
+    for action in available_actions:
+        new_board = result(board, action)
+        action_val, opponent_action = min_value(new_board)
+        if action_val > best_action_val:
+            best_action = action
+            best_action_val = action_val
+    return best_action_val, best_action
+
+
+def min_value(board):
+    if player(board) != O:
+        raise ValueError("Player should be O in min_value")
+    if terminal(board):
+        return (utility(board), None)
+    available_actions = actions(board)
+    best_action = None
+    best_action_val = 2
+    for action in available_actions:
+        new_board = result(board, action)
+        action_val, opponent_action = max_value(new_board)
+        if action_val < best_action_val:
+            best_action_val = action_val
+            best_action = action
+    return best_action_val, best_action
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if player(board) == X:
+        val, action = max_value(board)
+    else:
+        val, action = min_value(board)
+    return action
